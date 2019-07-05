961abe3b7da463d340569e2abe7ad9c6726f6f4d
dakudin <dakudin@gmail.com>
2019-07-05 19:06:21 +0300

    Small fix 2

--- config/params.php
+++ config/params.php
@@ -11,8 +11,5 @@ return [
     'white_house_clinic' => [
         'webflow_review_collection' => 'Reviews',
         'webflow_published_to_live' => false,
-        'google_mybusiness_api_credential' => 'd:\Work\Will\GoogleMyBusiness\whitehouse-244715-81f5724139d0.json',
-        'google_mybusiness_api_application_name' => 'Whitehouse',
-        'google_mybusiness_api_developer_key' => '81f5724139d01c0445b4d374081fcb7bc9ee1bb4'
     ],
 ];
