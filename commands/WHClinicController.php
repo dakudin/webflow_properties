d9a3258c4d808ae8319941935b5e134b9b18c980
dakudin <dakudin@gmail.com>
2019-07-05 18:51:51 +0300

    Add library for Google My Business API

--- commands/WHClinicController.php
+++ commands/WHClinicController.php
@@ -13,7 +13,6 @@ use yii\console\Controller;
 use app\models\GoogleReview;
 use yii\console\ExitCode;
 use app\components\whitehouse\clinic\WFReviewWorker;
-//use google\api\
 
 class WHClinicController extends Controller
 {
@@ -33,6 +32,7 @@ class WHClinicController extends Controller
      */
     public function actionIndex()
     {
+/*
         $this->WFReviewWorker = new WFReviewWorker(
             Yii::$app->params['white_house_clinic']['webflow_api_key'],
             Yii::$app->params['white_house_clinic']['webflow_review_collection'],
@@ -40,32 +40,76 @@ class WHClinicController extends Controller
         );
 
         //load all old reviews
-        $this->WFReviewWorker->loadAllReviews();
+        $this->WFReviewWorker->loadAllReviews();*/
 
         // update reviews and insert new ones
         $this->refreshReviews();
 
         //delete not exists reviews from WebFlow collection
-        $this->WFReviewWorker->deleteOldReviews();
+//        $this->WFReviewWorker->deleteOldReviews();
 
         return ExitCode::OK;
     }
 
+    protected function refreshReviews2()
+    {
+        /*$accounts previusly populate*/
+        /*(GMB - v4)*/
+/*        $credentials_f = Yii::$app->params['white_house_clinic']['google_mybusiness_api_credential'];
+        $client = new \Google_Client();
+        $client->setApplicationName(Yii::$app->params['white_house_clinic']['google_mybusiness_api_application_name']);
+        $client->setDeveloperKey(Yii::$app->params['white_house_clinic']['google_mybusiness_api_developer_key']);
+        $client->setAuthConfig($credentials_f);
+        $client->setScopes("https://www.googleapis.com/auth/plus.business.manage");
+        $client->setSubject($accounts->email );
+        $token = $client->refreshToken($accounts->refresh_token);
+        $client->authorize();
+
+        $locationName = "accounts/#######/locations/########";
+
+        $mybusinessService = new Google_Service_Mybusiness($client);
+
+        $reviews = $mybusinessService->accounts_locations_reviews;
+
+        do{
+            $listReviewsResponse = $reviews->listAccountsLocationsReviews($locationName, array('pageSize' => 100,
+                'pageToken' => $listReviewsResponse->nextPageToken));
+
+            $reviewsList = $listReviewsResponse->getReviews();
+            foreach ($reviewsList as $index => $review) {
+                //Accesing $review Object
+
+                $review->createTime;
+                $review->updateTime;
+                $review->starRating;
+                $review->reviewer->displayName;
+                $review->reviewReply->comment;
+                $review->getReviewReply()->getComment();
+                $review->getReviewReply()->getUpdateTime();
+
+
+            }
+
+        }while($listReviewsResponse->nextPageToken);*/
+    }
+
     /**
-     * Get all properties from Dezrez feed and store their to WebFlow
+     * Get all properties from Google My Business and store their to WebFlow
      */
     protected function refreshReviews()
     {
         $pageNumber = 0;
 
         $client = new \Google_Client();
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=/service-account.json');
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . Yii::$app->params['white_house_clinic']['google_mybusiness_api_credential']);
         $client->useApplicationDefaultCredentials();
         $client->addScope('https://www.googleapis.com/auth/plus.business.manage');
         $gmb = new \Google_Service_MyBusiness( $client );
         $accounts = $gmb->accounts->listAccounts()->getAccounts();
+        var_dump($accounts);
         $location = $gmb->accounts_locations->listAccountsLocations( $accounts[0]['name'] );
-        var_dump( $location->getLocations() );        $parser = new DezrezFeedParser();
+        var_dump( $location->getLocations() );
+/*        $parser = new DezrezFeedParser();
 
         do {
             $pageNumber++;
@@ -90,7 +134,7 @@ class WHClinicController extends Controller
             //break; //for testing
         } while($parser->getAllPropCount()>0 && $parser->getAllPropCount() >= $pageNumber * $this->propertiesPerPage);
 
-        echo "WebFlow: Inserted - " . $this->WFPropertyWorker->getInsertedCount() . "; Updated - " . $this->WFPropertyWorker->getUpdatedCount() . "\r\n";
+        echo "WebFlow: Inserted - " . $this->WFPropertyWorker->getInsertedCount() . "; Updated - " . $this->WFPropertyWorker->getUpdatedCount() . "\r\n";*/
     }
 
     /**
