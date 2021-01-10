808ab065084548d4fa020ec638d1e7a48b3c6f51
dakudin <dakudin@gmail.com>
2021-01-10 18:15:54 +0200

    OneAgency: Turn to live site

--- config/params.php
+++ config/params.php
@@ -13,8 +13,8 @@ return [
             'property_id_slug' => 'propertyid'
         ],
         'webflow_property_status_collection' => 'Property Statuses',
-        'webflow_published_to_live' => false,
-        'domain_name' => 'oneagency.webflow.io', //www.oneagencygroup.co.uk
+        'webflow_published_to_live' => true,
+        'domain_name' => 'www.oneagencygroup.co.uk', //www.oneagencygroup.co.uk // oneagency.webflow.io
     ],
     'white_house_clinic' => [
         'webflow_review_collection' => 'Reviews',
