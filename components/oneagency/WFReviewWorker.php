8be96f8ad08405fee1b3719846f520c68bf0b78f
dakudin <dakudin@gmail.com>
2021-01-14 11:35:18 +0200

    OneAgency: GMB fix field names

--- components/oneagency/WFReviewWorker.php
+++ components/oneagency/WFReviewWorker.php
@@ -47,11 +47,10 @@ class WFReviewWorker extends WFReviewWorkerBase
             'review-text-desktop' => "“" . StringHelper::truncate($commentInOneLine, static::$reviewDesktopLength, '...') . "”",
             'review-text-mobile' => "“" . StringHelper::truncate($commentInOneLine, static::$reviewMobileLength, '...') . "”",
             'creation-date' => $review->createTime, //\DateTime::createFromFormat('Y-m-d\TH:i:s.uP', $review->createTime)->format('m/d/Y'),
-            'clinic-location' => $review->locationName,
+            'location-name' => $review->locationName,
             'location-address' => $review->locationAddress,
             'location-primaryphone' => $review->locationPrimaryPhone,
             'name' => $review->reviewerName,
-            'anonymous-profile' => $review->reviewerIsAnonimous,
             'profile-image' => $review->reviewerIsAnonimous ? '' : $review->reviewerPhotoUrl,
             'slug' => $googleReviewId,
         ];
